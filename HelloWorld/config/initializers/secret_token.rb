f10f193453de04be2bb205d1523dbb45c5966b33
Christopher Wood <christopherwood07@gmail.com>
2013-01-19 00:51:12 -0500

    Pushing the first app skeleton

---
+++ HelloWorld/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '29d4db067ccfd183e9289e21a82f1d014c0b76f5b682c2dea2fcfc26403011d6ecca2464972da01825802ecc3926e25ad9a1dca81c798f9ca702757abbb1a6f8'
